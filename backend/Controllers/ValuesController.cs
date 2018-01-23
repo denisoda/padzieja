5a19bd40df3abb31018b64b354e0d596486fb6fe
Ilya <ilya.by@bk.ru>
2018-01-24 01:38:53 +0300

    Init

--- backend/Controllers/ValuesController.cs
+++ backend/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace backend.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value" + id;
         }
 
         // POST api/values
